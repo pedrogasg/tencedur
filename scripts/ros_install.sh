afd49a18e118b765502d37a90e20469edd71e332
FILLASTRE Pedro <az02290@FRLA000043.local>
2018-09-09 17:39:57 +0200

    Adding the mavros install

--- scripts/ros_install.sh
+++ scripts/ros_install.sh
@@ -11,8 +11,29 @@ if [ "$EUID" = 0 ]; then
 fi
 
 echo "${green}This script will install several components."
-echo "Please read license agreement for each component and continue only if you accept the license terms."
+echo "The components have diferents licenses check each one before using this repo."
 echo "ROS Kinetic  : http://www.ros.org/"
 echo "MAVROS       : http://github.com/mavlink/mavros"
 echo "raspicam_node: https://github.com/UbiquityRobotics/raspicam_node"
 echo "${reset}"
+
+# We are using the UbiquityRobotics ubuntu images so we not need to install ros
+
+# Setup your sources.list
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+# Set up your keys
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
+
+# Update package index
+sudo apt-get update
+
+# Install MAVROS packages
+sudo apt-get install -y ros-kinetic-mavros ros-kinetic-mavros-extras
+
+# MAVROS requires GeographicLib datasets starting v0.20 .
+sudo geographiclib-get-geoids egm96-5
+
+# Initialize rosdep
+sudo rosdep init
+rosdep update
