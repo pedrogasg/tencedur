4fc3b48469601559aa13889d05765468197b3e64
FILLASTRE Pedro <az02290@FRLA000043.local>
2018-09-22 01:34:57 +0200

    Adding first version of rc controller

---
+++ packages/rc_controller/include/rc_controller/rc_controller.h
@@ -0,0 +1,99 @@
+/**
+    rc_controller ROS node. 
+
+    Authors/maintainers: Pedro Fillastre
+*/
+
+#ifndef RC_CONTROLLER_RC_CONTROLLER_H
+#define RC_CONTROLLER_RC_CONTROLLER_H
+
+#include <math.h>
+
+#include <geometry_msgs/PoseStamped.h>
+#include <sensor_msgs/Joy.h>
+#include <sensor_msgs/Image.h>
+
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+#include <mavros_msgs/OverrideRCIn.h>
+
+#include <tf/tf.h>
+#include <tf2/buffer_core.h>
+#include <tf2/LinearMath/Transform.h>
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <angles/angles.h>
+#include <eigen_conversions/eigen_msg.h>
+
+#include <algorithm>
+#include <memory>
+
+namespace rc_controller
+{
+    class RCController
+    {
+        public:
+            RCController();
+            ~RCController() = default;
+
+            // public methods
+            bool init(ros::NodeHandle& nh);
+            bool arm();
+            void spin();
+
+            // subcriptions callback
+            void stateCallback(const mavros_msgs::State::ConstPtr &msg);
+
+    
+        private:
+            class RobotModel
+            {
+                public:
+                    RobotModel();
+                    ~RobotModel() = default;
+                    std::string getName()             override { return "Tencendur"; }
+                    std::string getOffboardModeName() override { return "OFFBOARD"; }
+                    bool init(ros::NodeHandle& nh) override;
+                private:
+                    // Ros publishers
+                    ros::Publisher rc_pub_;
+                    // RC parameters
+                    // Steering
+                    int rc_steer_trim_ = 1500;
+                    int rc_steer_dz_   = 30;
+                    int rc_steer_min_  = 1100;
+                    int rc_steer_max_  = 1900;
+                    // Throttle
+                    int rc_throttle_trim_ = 1500;
+                    int rc_throttle_dz_   = 30;
+                    int rc_throttle_min_  = 1100;
+                    int rc_throttle_max_  = 1900;
+            };
+
+        private:
+            float smoothFilter(float cur, float prev, float factor) const;
+
+        private:
+            // Ros parameters
+            float spin_rate_         = 50.0;
+            float smooth_factor_     = 0.15f;
+            float wait_for_services_ = 30.0;
+            float wait_for_service_  = 5.0
+            // Internal objects
+            std::unique_ptr<RobotModel> robot_model_;
+            // Saved states messages 
+            mavros_msgs::State current_state_;
+            geometry_msgs::PoseStamped current_pose_;
+            // Ros services 
+            ros::ServiceClient arming_service_;
+            ros::ServiceClient setmode_service_;
+            // Ros subscribers
+            ros::Subscriber state_sub_;
+            ros::Subscriber local_pose_sub_;
+            // Ros publishers
+            ros::Publisher local_pose_pub_;
+    };
+}
+#endif
---
+++ packages/rc_controller/src/rc_controller_node.cpp
@@ -0,0 +1,28 @@
+#include <ros/ros.h>
+#include "rc_controller/rc_controller.h"
+
+int main(int argc, char **argv)
+{
+    ROS_INFO("Starting rc_controller ROS node...");
+
+    ros::init(argc, argv, "rc_controller");
+    ros::NodeHandle nh("~");
+
+    rc_control::RCController controller;
+    if(!controller.init(nh))
+    {
+        ROS_ERROR("Could not initialize RCController node!");
+        return -1;
+    }
+
+    if(!controller.arm())
+    {
+        ROS_ERROR("Could not arm Robot FCU!");
+        return -1;
+    }
+
+    ROS_INFO("Rc_controller waiting for commands...");
+    controller.spin();
+
+    return 0;
+}
